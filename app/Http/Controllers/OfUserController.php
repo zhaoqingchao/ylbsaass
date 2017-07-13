bbbd53aaa931cf5b2610660ae36daca30df8e5d6
apple <waibaocn@163.com>
2017-07-13 17:01:24 +0800

    Revert "vendor"
    This reverts commit eab1cc0c7c9b2350995ab8b40cc299b4eb32c10f.

--- app/Http/Controllers/OfUserController.php
+++ app/Http/Controllers/OfUserController.php
@@ -16,7 +16,7 @@ use Flc\Alidayu\Requests\AlibabaAliqinFcSmsNumSend;
 use Validator;
 use Captcha;
 use Illuminate\Support\Facades\Session;
-
+ 
 
 class OfUserController extends Controller
 {
@@ -92,12 +92,10 @@ class OfUserController extends Controller
             return "账号密码错误，重新登录";
         }
     }
-    //添加员工用户信息
-    public  function AddUser(Request $request)
+    //添加用户信息
+    public  function AddUser($value='')
     {
         echo session('userid');
-        $adduser = O;
-
     }
     //阿里大鱼短信验证
     public function SendSms(Request $request)
